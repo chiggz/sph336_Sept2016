4b55ce96bb5a56d8eb9553154da323325e91cf53
koechpetersn <koechpetersn@gmail.com>
2016-11-17 15:37:40 +0300

    I added all the files

---
+++ koech_peter/decoder_2by4/monitor.h
@@ -0,0 +1,23 @@
+/*
+* Author: koech
+*/
+
+#include<iostream>
+#include<systemc>
+
+using namespace std;
+
+SC_MODULE(monitor){
+sc_in<bool> m_a, m_b, m_c, m_d, m_e, m_f;
+
+SC_CTOR(monitor){
+	SC_METHOD(monita);
+	sensitive<<m_c<<m_d<<m_e<<m_f;
+	dont_initialize();
+}
+
+void monita(void){
+cout<<"at "<<sc_time_stamp()<<" inputs are: "<<m_a<<" and "<<m_b<<" outputs are: "<<m_c<<" and "<<m_d<<" and "<<m_e<<" and "<<m_f<<endl;
+}
+};
+
